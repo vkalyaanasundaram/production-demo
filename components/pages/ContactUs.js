392d1a382e75a67695fe8d2c12773f6f6d207d9c
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-04 20:43:18 +0530

    image placeholder and shimmer added

--- components/pages/ContactUs.js
+++ components/pages/ContactUs.js
@@ -7,7 +7,32 @@ var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
 const ContactUs = (data) => {
   const contactData = data;
+
   mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+
+  const toBase64 = (str) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
+  const shimmer = (w, h) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(), // only run once
+    onLeave: ({ observe }) => observe(),
+  });
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -30,9 +55,14 @@ const ContactUs = (data) => {
             width={700}
             height={500}
             className={"image"}
+            quality={100}
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer(700, 475)
+            )}`}
           />
         </div>
-        {/* <div>{ReactHtmlParser(contactData.data.form)}</div> */}
+        <div>{ReactHtmlParser(contactData.data.form)}</div>
       </div>
       <div id="kapitus-map" className="w-full" style={{ height: 500 }}></div>
     </div>
