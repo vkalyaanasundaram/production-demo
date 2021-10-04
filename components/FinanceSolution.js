290af438ed1955bff1861df2d3b0b5d49b0c0f99
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-04 19:06:43 +0530

    finance solutions added

---
+++ components/FinanceSolution.js
@@ -0,0 +1,24 @@
+const FinanceSolution = () => {
+  return (
+    <div
+      fluid
+      className="xs: w-full px-5 md:w-full float-left px-10 justify-content-md-center mt-5 mb-5"
+    >
+      <div className="my-3">
+        <h2 className="h2-title">FIND YOUR FINANCING SOLUTION</h2>
+        <div>
+          With hundreds of financing options available for today’s business
+          owner, determining the right financing product for you and your
+          business can be overwhelming. By answering a few questions about your
+          present circumstances and future plans, your current financial
+          situation and your financing preferences, you can use our matching
+          tool to quickly find out the best solution for your needs. You will
+          also be able to see what financing products business owners in similar
+          situations and with similar preferences chose.
+        </div>
+      </div>
+      <button className="btn-apply">APPLY NOW</button>
+    </div>
+  );
+};
+export default FinanceSolution;
