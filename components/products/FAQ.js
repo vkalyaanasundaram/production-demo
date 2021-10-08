9ba55810e0859c667ff0fcfc504614c2b1d14c59
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-08 18:35:19 +0530

    faq, how to apply, who should content added

--- components/products/FAQ.js
+++ components/products/FAQ.js
@@ -5,6 +5,7 @@ import ReactHtmlParser, { htmlparser2 } from "react-html-parser";
 import useSWR from "swr";
 import { request } from "graphql-request";
 import { useRouter } from "next/router";
+import { contentNav } from "../../styles/Home.module.css";
 
 const fetcher = (query) =>
   request(process.env.WORDPRESS_GRAPHQL_ENDPOINT, query);
@@ -48,39 +49,44 @@ const FAQ = () => {
   //   console.log(faq)
   return (
     <>
-      <div className="w-full md:w-3/5 mx-auto p-8">
-        <div className="shadow-md">
-          {faq?.map((value, key) => (
-            // console.log(data);
+      <div className={contentNav}>
+        <div className="container py-5 px-5">
+          <h3>FAQs</h3>
+          <div className="w-full md:w-3/5 mx-auto p-8">
+            <div className="shadow-md">
+              {faq?.map((value, key) => (
+                // console.log(data);
 
-            <div
-              className="tab w-full overflow-hidden border-t accordion"
-              key={key}
-              id={`tabheading-${key}`}
-              onClick={(event) => handleClick(key)}
-            >
-              <input
-                className="absolute opacity-0 accordion"
-                id={`tab-${key}`}
-                type="radio"
-                name="tabs2"
-              />
-              <label
-                className="block p-5 leading-normal cursor-pointer accordion text-blue-600"
-                htmlFor={`tab-single-${key}`}
-                id={`tab-single-${key}`}
-              >
-                {value.question}
-              </label>
-              {showCurrent ? (
-                <div className="tab-content overflow-hidden border-l-2 bg-gray-100 leading-normal">
-                  <div className="p-5 text-sm text-gray-600">
-                    {ReactHtmlParser(value.answer)}
-                  </div>
+                <div
+                  className="tab w-full overflow-hidden border-t accordion"
+                  key={key}
+                  id={`tabheading-${key}`}
+                  onClick={(event) => handleClick(key)}
+                >
+                  <input
+                    className="absolute opacity-0 accordion"
+                    id={`tab-${key}`}
+                    type="radio"
+                    name="tabs2"
+                  />
+                  <label
+                    className="block p-5 leading-normal cursor-pointer accordion text-blue-600"
+                    htmlFor={`tab-single-${key}`}
+                    id={`tab-single-${key}`}
+                  >
+                    {value.question}
+                  </label>
+                  {showCurrent ? (
+                    <div className="tab-content overflow-hidden border-l-2 bg-gray-100 leading-normal">
+                      <div className="p-5 text-sm text-gray-600">
+                        {ReactHtmlParser(value.answer)}
+                      </div>
+                    </div>
+                  ) : null}
                 </div>
-              ) : null}
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </>
--- components/products/HowToApply.js
+++ components/products/HowToApply.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useInView } from "react-cool-inview";
 import ReactHtmlParser, { htmlparser2 } from "react-html-parser";
+import { contentNav } from "../../styles/Home.module.css";
 
 import { useRouter } from "next/router";
 
 const HowToApply = ({ data }) => {
   //   console.log(data)
-  return <div className="container">{ReactHtmlParser(data)}</div>;
+  return (
+    <>
+      <div className={contentNav}>
+        <div className="container py-10 px-5">
+          <div className="container">{ReactHtmlParser(data)}</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HowToApply;
