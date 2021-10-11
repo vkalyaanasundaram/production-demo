00713082bcc3256c547d5a05b096f02fa736486b
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-11 16:02:40 +0530

    path module extension added to check chunks performance.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['kap-staging.us'],
+    domains: ["kap-staging.us"],
   },
   env: {
     WP_HEADLESS_SECRET: "adb05f33-6f44-4433-9e79-9f995018b28a",
@@ -9,5 +11,14 @@ module.exports = {
     WORDPRESS_URL: "https://kap-staging.us/",
     MAPBOX_TOKEN:
       "pk.eyJ1Ijoia2FwaXR1cyIsImEiOiJjanR5and6MjYwMXltNDN0MWxnNTFoMGxpIn0.rbpYqdyv1o5OzsDLva85Sg",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "babel-loader",
+      },
+    ],
   },
 };
