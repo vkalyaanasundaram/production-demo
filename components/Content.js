d786457329a5132d49ef50c767fb81c4b889b6be
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-13 14:19:15 +0530

    home div moved to conent.js

--- components/Content.js
+++ components/Content.js
@@ -27,34 +27,42 @@ const Content = ({ data }) => {
   // console.log(data);
 
   return (
-    <section className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
-      {data?.map((value, key) => (
-        <div
-          className="shadow-md rounded-md overflow-hidden dark:bg-red-100 dark:text-black"
-          key={key}
-        >
-          <div className="grid place-items-center w-full text-right">
-            <Image
-              src={value?.svgIcon?.sourceUrl}
-              width="100"
-              height="100"
-              alt=""
-              objectFit="cover"
-              quality={100}
-              placeholder="blur"
-              blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                shimmer(700, 475)
-              )}`}
-            />
-          </div>
-          <h2 className="text-xl font-semibold mb-2 text-center mx-8">
-            {ReactHtmlParser(value?.cardTitle)}
-          </h2>
-          <div className="place-items-center">
-            <p className="mb-4 p-5">{value?.cardContent}</p>
+    <section>
+      <div className="xs:w-full container px-5 mt-10 mb-10 mx-auto">
+        <div>
+          <div className="container">
+            <section className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
+              {data?.map((value, key) => (
+                <div
+                  className="shadow-md rounded-md overflow-hidden dark:bg-red-100 dark:text-black"
+                  key={key}
+                >
+                  <div className="grid place-items-center w-full text-right">
+                    <Image
+                      src={value?.svgIcon?.sourceUrl}
+                      width="100"
+                      height="100"
+                      alt=""
+                      objectFit="cover"
+                      quality={100}
+                      placeholder="blur"
+                      blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                        shimmer(700, 475)
+                      )}`}
+                    />
+                  </div>
+                  <h2 className="text-xl font-semibold mb-2 text-center mx-8">
+                    {ReactHtmlParser(value?.cardTitle)}
+                  </h2>
+                  <div className="place-items-center">
+                    <p className="mb-4 p-5">{value?.cardContent}</p>
+                  </div>
+                </div>
+              ))}
+            </section>
           </div>
         </div>
-      ))}
+      </div>
     </section>
   );
 };
