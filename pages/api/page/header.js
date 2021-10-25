5dbac30f3d41a5194cef5a14a0a5cc598d060812
Vinoth <vkalyaanasundaram@kapitus.com>
2021-10-25 11:46:38 +0530

    all cache and headers to check performance

--- pages/api/page/header.js
+++ pages/api/page/header.js
@@ -3,10 +3,10 @@ export default async (req, resp) => {
     query: { slug },
   } = req;
 
-  resp.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
+  // resp.setHeader(
+  //   "Cache-Control",
+  //   "public, s-maxage=10, stale-while-revalidate=59"
+  // );
 
   const errorCode = resp.ok ? false : resp.statusCode;
 
@@ -27,20 +27,20 @@ export default async (req, resp) => {
             }
         }
       }`;
-  
-    const data = await fetch(process.env.WORDPRESS_GRAPHQL_ENDPOINT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: QUERY_SINGLE_POST,
-        variables: {
-          id: slug,
-        },
-      }),
-    });
 
-    //   console.log(json);
-    const json = await data.json();
-    // console.log(json);
-    resp.json(json.data.menu);
+  const data = await fetch(process.env.WORDPRESS_GRAPHQL_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: QUERY_SINGLE_POST,
+      variables: {
+        id: slug,
+      },
+    }),
+  });
+
+  //   console.log(json);
+  const json = await data.json();
+  // console.log(json);
+  resp.json(json.data.menu);
 };
